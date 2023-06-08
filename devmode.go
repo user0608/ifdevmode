315f4e9b08ac396565bdcdace0dfc2eded304fe1
Kevin Saucedo <developer2@sfperusac.com>
2023-06-08 17:15:28 -0500

    funcion to check if is dev mode

--- devmode.go
+++ devmode.go
@@ -30,23 +30,26 @@ type options struct {
 
 type option func(o *options)
 
-func WithSyncExecution() option           { return func(o *options) { o.issync = true } }
+func WithSyncExecution() option { return func(o *options) { o.issync = true } }
+
 func WithExecuteOn(fn func() bool) option { return func(o *options) { o.executeif = fn } }
+
+func Yes() bool {
+	var isdevmode bool
+	for _, mode := range devmode {
+		if mode != "" {
+			if _, ok := positives[mode]; ok {
+				isdevmode = true
+				break
+			}
+		}
+	}
+	return isdevmode
+}
 
 func Do(fn func(), withs ...option) {
 	var opts options
-	opts.executeif = func() bool {
-		runFunc := false
-		for _, mode := range devmode {
-			if mode != "" {
-				if _, ok := positives[mode]; ok {
-					runFunc = true
-					break
-				}
-			}
-		}
-		return runFunc
-	}
+	opts.executeif = Yes
 	for _, wth := range withs {
 		wth(&opts)
 	}
@@ -58,4 +61,3 @@ func Do(fn func(), withs ...option) {
 		}
 	}
 }
-
